c1620c5bd3ed095cf860ed39cd6ccd22a4a43c30
Daniel Okafor <bucheed@gmail.com>
2025-02-21 06:40:10 +0100

    token update

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const originMeta = document.createElement('meta');
-originMeta.httpEquiv = 'origin-trial';
-originMeta.content = import.meta.env.REACT_APP_TRANSLATOR_API_KEY;
-document.head.append(originMeta);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
